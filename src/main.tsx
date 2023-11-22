1eb4882d45c2ad2365b0f571393a1852505a331f
withsang <jsa5115@naver.com>
2023-11-22 21:01:55 +0900

    refactor(recoil): remove recoil

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "@/router";
-import { RecoilRoot } from "recoil";
 
 createRoot(document.getElementById("root")!).render(
-  <RecoilRoot>
-    <RouterProvider router={router} />,
-  </RecoilRoot>,
+  <RouterProvider router={router} />,
 );
