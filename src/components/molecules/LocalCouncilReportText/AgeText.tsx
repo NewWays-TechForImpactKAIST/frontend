e645e1cc40972870042390a473d426cf0cbe5d12
withsang <jsa5115@naver.com>
2023-11-24 15:41:15 +0900

    fix(text): add bold to age

--- src/components/molecules/LocalCouncilReportText/AgeText.tsx
+++ src/components/molecules/LocalCouncilReportText/AgeText.tsx
@@ -71,7 +71,7 @@ export const AgeText = ({
         후보로 나와 <Text strong>{ageHistogramParagraph.electedCount}</Text>명이{" "}
         당선됐어요. 당선자의 20%가{" "}
         <Text strong>{ageHistogramParagraph.firstQuintile}</Text>세 미만, 20%가{" "}
-        {ageHistogramParagraph.lastQuintile}세 이상이에요.
+        <Text strong>{ageHistogramParagraph.lastQuintile}</Text>세 이상이에요.
         <br />
         <br />
         참고로 다양성 지표 전국 1위는 전체 인원의 20%가{" "}
