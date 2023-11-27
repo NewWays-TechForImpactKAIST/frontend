da97eacea3444860217797db7f0c17e2a991c3bf
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-11-28 04:06:05 +0900

    feat(add) test

--- src/components/molecules/MetroCouncilReportText/GenderText.tsx
+++ src/components/molecules/MetroCouncilReportText/GenderText.tsx
@@ -22,8 +22,22 @@ export const GenderText = ({ variation = 1, data = undefined }: Props) => {
   if (variation === 1)
     return (
       <Paragraph>
-        이 지역의 성별 다양성 지표의 값은{" "}
-        <Text strong>{genderDiversityIndex}</Text>입니다.
+        {"2020"}년 지방선거 당선자의 성비는{" "}
+        <Text strong>{"변화하지 않았습니다."}</Text> {"2024"}년 지방선거에서{" "}
+        {"충청남도"}의 당선자의 성별은 남성{" "}
+        <Text strong>
+          {5}명({"60%"})
+        </Text>
+        , 여성{" "}
+        <Text strong>
+          {3}명({"40%"})
+        </Text>
+        입니다. <br /> <br /> 전국 지역 의회는 평균적으로 남성이{" "}
+        <Text strong>{"60%"}</Text>, 여성이 <Text strong>{"40%"}</Text>를
+        차지하고 있습니다. <br /> <br />
+        해당 지역 의원 10명 중 남성은 <Text strong>{5}</Text>명, 여성은{" "}
+        <Text strong>{3}</Text>명인 정도이기 때문에,{" "}
+        <Text strong>{"전국 평균 대비 높은 수준입니다."}</Text>
       </Paragraph>
     );
   return <Paragraph>존재하지 않는 템플릿입니다.</Paragraph>;
