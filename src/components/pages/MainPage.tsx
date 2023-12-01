8825e29d5b7b65774fd570bd668972c351cfe73b
happycastle <41810556+happycastle114@users.noreply.github.com>
2023-12-01 10:09:52 +0900

    Add: Validator

--- src/components/pages/MainPage.tsx
+++ src/components/pages/MainPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { Divider } from "antd";
 import { Layout } from "@/components/templates";
 import { Card } from "@/components/atoms";
@@ -10,8 +10,30 @@ import {
   TabSelector,
 } from "@/components/organisms";
 
+const BANNED_METRO = ["제주특별자치도", "세종특별자치시"];
+
+const BANNED_LOCAL = ["군위군"];
+
+const vaildateLocalAndMetroName = (
+  reportType: string,
+  metroName: string,
+  localName: string,
+) => {
+  if (
+    reportType === "localCouncil" &&
+    (BANNED_LOCAL.includes(localName) || BANNED_METRO.includes(metroName))
+  ) {
+    return false;
+  }
+
+  return true;
+};
 const MainPage = () => {
-  const { reportType = "" } = useParams();
+  const { reportType = "", metroName = "", localName = "" } = useParams();
+  const navigate = useNavigate();
+  if (!vaildateLocalAndMetroName(reportType, metroName, localName)) {
+    navigate("/localCouncil", { replace: true });
+  }
 
   return (
     <Layout>
